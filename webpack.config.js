3ac63bacc8c45cb0a1097ce5ae9103a34bbf3bed
iamdevlinph <iamdevlinph@gmail.com>
2017-01-28 22:20:07 +0800

    [lint]

--- webpack.config.js
+++ webpack.config.js
@@ -5,25 +5,25 @@ var BUILD_DIR = path.resolve(__dirname, 'public');
 var APP_DIR = path.resolve(__dirname, 'src');
 
 var config = {
-  entry: APP_DIR + '/index.js',
-  output: {
-    path: BUILD_DIR,
-    filename: 'bundle.js'
-  },
-  module: {
-      loaders: [
-          {
-            test: /\.js/,
-            include: APP_DIR,
-            loaders: ['babel'],
-          },
-          {
-              test: /\.css/,
-              include: APP_DIR,
+    entry: APP_DIR + '/index.js',
+    output: {
+        path: BUILD_DIR,
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js/,
+                include: APP_DIR,
+                loaders: ['babel'],
+            },
+            {
+                test: /\.css/,
+                include: APP_DIR,
                 loaders: ['style-loader', 'css-loader'],
-          }
-      ]
-  }
+            }
+        ]
+    }
 };
 
 module.exports = config;
