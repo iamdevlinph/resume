0fe840120b889042a98e57abf4be04440b1c2011
iamdevlinph <iamdevlinph@gmail.com>
2017-01-28 20:47:07 +0800

    [Navbar] Working SCSS [Updated] webpack.config.dev.js

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -111,7 +111,8 @@ module.exports = {
                     /\.(js|jsx)$/,
                     /\.css$/,
                     /\.json$/,
-                    /\.svg$/
+                    /\.svg$/,
+                    /\.scss$/
                 ],
                 loader: 'url',
                 query: {
