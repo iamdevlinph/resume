3fc33a29f09505b935e44404ece99405d78bd61b
iamdevlinph <iamdevlinph@gmail.com>
2017-01-29 18:22:58 +0800

    [About] Section [Content] Minor changes to css [Added] Variable.scss but not working [Added] profile picture [Added] Open sans font to index.html

--- src/Components/Content/About/About.js
+++ src/Components/Content/About/About.js
@@ -11,49 +11,44 @@ class About extends Component {
                         <div className="profile">
                             <div className="row">
                                 <div className="col s5">
-                                    <div className="profile-photo"><img src="http://rscardwp.px-lab.com/wp-content/uploads/2015/11/robert_WP-1-299x347.jpg" alt="" /></div>
+                                    <div className="profile__photo"><img src={require('./img/profile-picture.jpg')} alt="Profile"/></div>
                                 </div>
                                 <div className="col s7">
-                                    <div className="profile-info">
-                                        <div className="profile-items clearfix">
-                                            <div className="profile-preword"><span>Hello</span></div>
-
-                                        </div>
-                                        <h1 className="profile-title">
-                                            <span>I'm</span> Robert Smith </h1>
-                                        <h2 className="profile-position">Developer and Startup entrepreneur</h2>
+                                    <div className="profile__info">
+                                        {/*<div className="profile__items clearfix">
+                                            <div className="profile__preword"><span>Hello</span></div>
+                                        </div> */}
+                                        <h1 className="profile__title">
+                                            <span>Devlin</span> Pajaron</h1>
+                                        <h2 className="profile__position">Full Stack Developer</h2>
                                     </div>
-                                    <ul className="profile-list">
+                                    <ul className="profile__list">
                                         <li className="clearfix">
                                             <strong className="title">Age</strong>
-                                            <span className="cont">29</span>
+                                            <span className="cont">21</span>
                                         </li>
                                         <li className="clearfix">
                                             <strong className="title">Address</strong>
-                                            <span className="cont">24058, Belgium, Brussels, Liutte 27, BE</span>
+                                            <span className="cont">Cebu City, Philippines</span>
                                         </li>
                                         <li className="clearfix">
                                             <strong className="title">E-mail</strong>
-                                            <span className="cont">robertsmith@company.com</span>
+                                            <span className="cont">devlinpajaron@gmail.com</span>
                                         </li>
                                         <li className="clearfix">
-                                            <strong className="title">Phone</strong>
-                                            <span className="cont">+1 256 254 84 56</span>
+                                            <strong className="title">Mobile</strong>
+                                            <span className="cont">+63 943 230 2440</span>
                                         </li>
-                                        <li className="clearfix">
-                                            <strong className="title">Freelance</strong>
-                                            <span className="cont">till March 25, 2016</span>
-                                        </li>
-                                        <li className="clearfix">
+                                        {/*<li className="clearfix">
                                             <strong className="title"><span className="button">Vacation</span></strong>
                                             <span className="cont">
                                                 <i className="rsicon rsicon-calendar"></i>till April 15, 2016</span>
-                                        </li>
+                                        </li>*/}
                                     </ul>
                                 </div>
                             </div>
                         </div>
-                        <div className="profile-social">
+                        <div className="profile__social">
                             <ul className="social">
                                 <li><a href="https://www.twitter.com/" target="_blank"><i className="rsicon rsicon-twitter"></i></a></li>
                                 <li><a href="https://www.facebook.com/" target="_blank"><i className="rsicon rsicon-facebook"></i></a></li>
@@ -67,10 +62,9 @@ class About extends Component {
                     </div>
 
                     <div className="section__text">
-                        <p>Hello! I’m Robert Smith. Senior Web Developer specializing in front end development. Experienced
-                                with all stages of the development cycle for dynamic web projects. Well-versed in numerous programming
-                                languages including JavaScript, SQL, and C. Stng background in project management and customer relations.</p>
-
+                        <p>
+                            Hello! I'm Devlin Pajaron. A fresh graduate that is off to learn new things.
+                        </p>
                     </div>
                 </section>
             </div>
