e00dd6308c63714d29d2076ecbcb984deb20a4a3
iamdevlinph <iamdevlinph@gmail.com>
2017-02-19 17:38:10 +0800

    [API] Fetch skills from API [Skills] Add React

--- src/Components/Content/Skills/Skills.js
+++ src/Components/Content/Skills/Skills.js
@@ -3,12 +3,23 @@ import React, { Component } from 'react';
 import './Skills.scss';
 
 class Skills extends Component {
+    constructor() {
+        super();
+        this.state = { skillsData: [] };
+    }
+    componentDidMount() {
+        fetch(`https://private-0b7e9-iamdevlinph.apiary-mock.com/skills`).then(function (data) {
+            return data.json();
+        }).then( (data) => {
+            this.setState({ skillsData: data });
+        });
+    }
     render() {
-        var skillList = require('./data/skills.json').map(function(skill, index) {
+        var skillList = this.state.skillsData.map(function(skill, index) {
             return (
                 <li key={index}>
                     <div className="skills__logo">
-                        <img src={require(skill.img_path)} alt="Skill"/>
+                        <img src={skill.img_base64} alt="Skill"/>
                     </div>
                     <div className="skills__name">
                         <h4>{skill.name}</h4>
