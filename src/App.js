627dbbf9f3e295e613a40c372fc064dc41c0ddf4
iamdevlinph <iamdevlinph@gmail.com>
2017-04-20 00:07:27 +0800

    v2 skills component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import React, { Component } from 'react';
 // import Footer from './Components/Footer/Footer';
 
 import About from './Components/About/About';
+import Skills from './Components/Skills/Skills';
 
 import './css/bootstrap.scss';
 import './css/font.scss';
@@ -41,55 +42,7 @@ class App extends Component {
                                 <section className="ofsInBottom">
                                     <div className="resume">
                                         {/*<!-- START SKILLS -->*/}
-                                        <div className="resume-card">
-                                            <div className="main-title">
-                                                <h1>skills</h1>
-                                                <hr />
-                                            </div>
-                                            <div className="content">
-                                                <div className="block-content ">
-                                                    <div className="services">
-                                                        <div className="row ">
-                                                            <div className="block service tCenter">
-                                                                <div className="col-md-4">
-                                                                    <div className="ico">
-                                                                        <i className="icon-lifebuoy"></i>
-                                                                    </div>
-                                                                    <div className="det">
-                                                                        <h3>Great Support</h3>
-                                                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                                                                accusantium.
-                                                            </p>
-                                                                    </div>
-                                                                </div>
-                                                                <div className="col-md-4">
-                                                                    <div className="ico">
-                                                                        <i className="icon-print-1"></i>
-                                                                    </div>
-                                                                    <div className="det">
-                                                                        <h3>Print &amp; Branding</h3>
-                                                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                                                                accusantium.
-                                                            </p>
-                                                                    </div>
-                                                                </div>
-                                                                <div className="col-md-4">
-                                                                    <div className="ico">
-                                                                        <i className="icon-megaphone"></i>
-                                                                    </div>
-                                                                    <div className="det">
-                                                                        <h3>Marketing</h3>
-                                                                        <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-                                                                accusantium.
-                                                            </p>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
+                                        <Skills />
                                         {/*<!-- END SKILLS -->*/}
 
                                         {/*<!-- START EXPERIENCE -->*/}
