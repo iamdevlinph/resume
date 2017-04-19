a98bfcd9ec2bd8797be0a16caafd949afb1cfa7b
iamdevlinph <iamdevlinph@gmail.com>
2017-04-19 15:59:45 +0800

    Init v3

---
+++ v3/js/script.js
@@ -0,0 +1,12 @@
+(function($) {
+  "use strict";
+
+//------------------------------------- Waiting for the entire site to load ------------------------------------------------//
+
+jQuery(window).load(function() { 
+		jQuery("#loaderInner").fadeOut(); 
+		jQuery("#loader").delay(200).fadeOut("slow"); 
+});
+
+
+})(jQuery);
