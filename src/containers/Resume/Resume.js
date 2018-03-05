c7a9b6cb4739784dc793dfe2b73cddfb41d11cfb
iamdevlinph <iamdevlinph@gmail.com>
2018-03-06 00:56:06 +0800

    Start resume grid  responsive layout

--- src/containers/Resume/Resume.js
+++ src/containers/Resume/Resume.js
@@ -2,13 +2,16 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { About, Education, Experience, Portfolio, Skills } from '../index';
-import './resume-grid.css';
 
 const ResumeGrid = styled.div`
   display: grid;
   grid-area: main-area;
   grid-template-columns: 280px auto;
-  grid-column-gap: 10px;
+  grid-column-gap: 20px;
+  @media only screen and (max-width: 599px) {
+    grid-template-columns: auto;
+    grid-row-gap: 20px;
+  }
 `;
 
 class ResumeContainer extends React.Component {
--- src/routes/ResumeLayout/ResumeLayout.js
+++ src/routes/ResumeLayout/ResumeLayout.js
@@ -2,18 +2,25 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-const ResumeGridLayout = styled.div`
+const ResumeLayoutGrid = styled.div`
   display: grid;
   grid-template-areas: "l-pad main-area r-pad";
   grid-template-columns: auto 1200px auto;
+  @media only screen and (max-width: 1200px) and (min-width: 600px) {
+    grid-template-columns: 0px auto 0px;
+  }
+  @media only screen and (max-width: 599px) {
+    grid-template-areas: "main-area";
+    grid-template-columns: auto;
+  }
 `;
 
 class ResumeLayout extends React.Component {
   render() {
     return (
-      <ResumeGridLayout>
+      <ResumeLayoutGrid>
         {this.props.children}
-      </ResumeGridLayout>
+      </ResumeLayoutGrid>
     );
   }
 }
