f4a0b2a57fb56b7fd03eee14df997565aa14c4da
iamdevlinph <iamdevlinph@gmail.com>
2018-03-29 00:18:28 +0800

    Switch to BrowserRouter

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import ResumeLayout from './ResumeLayout/ResumeLayout';
@@ -17,11 +17,11 @@ const AppRoute = ({ component: Component, layout: Layout, ...rest }) => (
 );
 
 export default () => (
-  <HashRouter >
+  <BrowserRouter >
     <Switch>
       <AppRoute exact path="/" layout={ResumeLayout} component={loadableCons.Resume} />
     </Switch>
-  </HashRouter >
+  </BrowserRouter >
 );
 
 AppRoute.propTypes = {
